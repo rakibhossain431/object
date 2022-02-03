57ecd08260843d7602258c0706ac4b8e165b8b41
rakibhossain431 <rakibjp431@gmail.com>
2022-02-04 04:28:05 +0600

    pactic code

---
+++ app.js
@@ -0,0 +1,41 @@
+let x = 5;
+let y = 5;
+let z = 6;
+
+
+console.log( (x==y));
+console.log( (x==z));
+
+const cars =["rakib","hassain"]
+console.log(cars[1]);
+
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
+console.log(person.firstName + " prebareat color is " +person.eyeColor);
+
+
+ let n = 5;
+ let m = 5;
+ let a = 5;
+ let d = 5;
+
+ console.log(typeof n);
+ console.log(typeof m);
+ console.log(typeof a);
+ console.log(typeof d);
+
+
+ function slecp(){
+     console.log('josim "is" good boy');
+ }
+ slecp();
+ console.log("My name is rakib");
+
+
+function sleeping(names,time){
+    console.log(names + "Rakib is good boy" + "He sleeping at " + time);
+}
+
+sleeping("korim","10pm");
+
+sleeping("rohim","12pm");
